c5b43064f39d921a0fff510d57a496fadd9a112f
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-29 10:57:18 +0100

    added- hamburger nav

--- Frontend-final-project/src/components/Navbar.jsx
+++ Frontend-final-project/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { linkData } from "../data/db";
 import { Link, NavLink } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,36 +15,70 @@ export default function Navbar() {
       console.log("user started scrolling");
     }
     else {
-      setIsFloat(false);
+      // setIsFloat(false);
       console.log("user stopped scrolling");
     }
   }, [])
 
   return (
-    <nav className={`flex  ${isFloat ? "fixed" : "relative"} top-0 w-full bg-white z-50 justify-between cursor-pointer items-center px-6 md:px-24 py-4`}>
-      {/* Logo Section */}
-      <Link to={"/"} className="text-xl text-primary-100 font-semibold">
-        TOPCAR
-      </Link>
-      <div className="justify-between hidden text-black text-sm lg:flex w-[30%] items-center">
+    <div className={`flex flex-col z-50 w-full ${isFloat ? "fixed bg-green-500 top-0" : "relative"} `}>
+      <nav className={`flex  w-full bg-white z-50 justify-between cursor-pointer items-center px-6 md:px-24 py-4`}>
+        {/* Logo Section */}
+        <Link to={"/"} className="text-xl text-primary-100 font-semibold">
+          TOPCAR
+        </Link>
+        <div className="justify-between hidden text-black text-sm lg:flex w-[30%] items-center">
+          {
+            linkData.map((link) => (
+              <NavLink
+                key={link.id}
+                to={link.path}
+                className={({ isActive }) =>
+                  isActive ? "border-b-[2px] font-semibold border-primary-100" : ""
+                }
+              >
+                {link.label}
+              </NavLink>
+            ))
+          }
+        </div>
+        <Link to={"/contact"} className="bg-primary-100 lg:block hidden px-4 text-sm font-semibold text-white rounded-lg py-2">
+          Contact Us
+        </Link>
+        <p
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="bg-primary-100 lg:hidden block text-white text-3xl p-1">
+         {
+          isMenuOpen ? (<IoClose />) : ( <BiMenu />)
+         }
+        </p>
+        {/* Mobile Menu */}
+
+      </nav>
+
+      <div className="z-50">
         {
-          linkData.map((link) => (
-            <NavLink
-              key={link.id}
-              to={link.path}
-              className={({ isActive }) =>
-                isActive ? "border-b-[2px] font-semibold border-primary-100" : ""
+          isMenuOpen && (
+            <div className="justify-between lg:hidden gap-3 border-2 bg-gray-300 
+             py-5 text-black text-sm flex-col flex w-[100%] items-center">
+              {
+                linkData.map((link) => (
+                  <NavLink
+                    key={link.id}
+                    to={link.path}
+                    className={({ isActive }) =>
+                      isActive ? "border-b-[2px] font-semibold border-primary-100" : ""
+                    }
+                  >
+                    {link.label}
+                  </NavLink>
+                ))
               }
-            >
-              {link.label}
-            </NavLink>
-          ))
+            </div>
+          )
         }
       </div>
-      <Link to={"/contact"} className="bg-primary-100 px-4 text-sm font-semibold text-white rounded-lg py-2">
-        Contact Us
-      </Link>
-    </nav>
+    </div>
   )
 
   // return (
